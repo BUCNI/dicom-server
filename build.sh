439fcd575eb000ff9e522cc5127bcf273037c541
Oliver Josephs <o.josephs@fil.ion.ucl.ac.uk>
2019-06-18 14:19:11 +0100

    build with systemd commands

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 # remove old version
-sudo docker stop -t0 bdsc
+sudo systemctl stop bucni_dicom.service
+#sudo docker stop -t0 bdsc
 sudo docker rm bdsc
 sudo docker rmi bds
 # build and run new version
 sudo docker build --pull --no-cache -t bds .
 sudo docker create --name=bdsc -v /:/mnt --net=host bds
-sudo docker start bdsc
+sudo systemctl start bucni_dicom.service
+#sudo docker start bdsc
 sudo docker exec -it bdsc bash
